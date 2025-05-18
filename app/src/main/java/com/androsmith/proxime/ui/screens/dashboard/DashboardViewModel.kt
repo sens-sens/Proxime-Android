12e835e4fc2108bdc2bd68828c0bfa2fcedb6c9d
sens <senthilraja1919@gmail.com>
2025-05-18 19:24:33 +0530

    UI Optimization - Devices

--- app/src/main/java/com/androsmith/proxime/ui/screens/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/androsmith/proxime/ui/screens/dashboard/DashboardViewModel.kt
@@ -2,14 +2,12 @@ package com.androsmith.proxime.ui.screens.dashboard
 
 import android.annotation.SuppressLint
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.androsmith.proxime.data.bluetooth.SensorBluetoothService
 import com.androsmith.proxime.data.model.SensorData
 import com.androsmith.proxime.domain.model.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jakarta.inject.Inject
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 
 @SuppressLint("MissingPermission")
 @HiltViewModel
